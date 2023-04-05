bbae4c026051e13eb989fcf7d6d2ca4d5cc9fb32
Jose Hernandez <jose_r_hernandez@progressive.com>
2023-04-05 08:43:15 -0400

    initial setup done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoadingService} from './loading/loading.service';
+import {MessagesService} from './messages/messages.service';
+import {AuthStore} from './services/auth.store';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive-angular-course';
+export class AppComponent implements  OnInit {
+
+    constructor(public auth: AuthStore) {
+
+    }
+
+    ngOnInit() {
+
+
+    }
+
+  logout() {
+        this.auth.logout();
+
+  }
+
 }
